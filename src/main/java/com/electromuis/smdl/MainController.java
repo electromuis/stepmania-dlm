8991ae0a4f50ec122920852c1510261df7022445
Anton <a.grootes@faceworks.nl>
2017-08-23 16:59:01 +0200

    Added fx css and controller

--- src/main/java/com/electromuis/smdl/MainController.java
+++ src/main/java/com/electromuis/smdl/MainController.java
@@ -1,16 +1,32 @@
 package com.electromuis.smdl;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by anton on 23-8-17.
  */
 public class MainController {
+    @FXML
+    TableView packsTable;
+
+    Map<String, Pack> packs = new HashMap<String, Pack>();
+    ObservableList<Pack> packList = FXCollections.observableArrayList();
 
     @FXML
     private void initialize() {
-
+        FilteredList<Pack> filteredPacks = new FilteredList<>(packList, p -> true);
+        SortedList<Pack> sortedPacks = new SortedList<>(filteredPacks);
+        sortedPacks.comparatorProperty().bind(packsTable.comparatorProperty());
+        packsTable.setItems(sortedPacks);
     }
 }
